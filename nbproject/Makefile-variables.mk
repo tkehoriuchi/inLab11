2e67a45beb097ad805677777ffdeb20b4b27ce12
Tyler Horiuchi <thoriuchi@mymail.mines.edu>
2020-04-02 08:29:35 -0600

    first commit

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=inLab11.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/inLab11.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=inlab11.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/inlab11.x.tar
